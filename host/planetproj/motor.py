98246abd7c0f92d0c021a4220a7b3ee31b37345f
Sugizaki Yukimasa <ysugi@idein.jp>
2018-10-22 22:49:23 +0900

    host: motor: Add more asserts

--- host/planetproj/motor.py
+++ host/planetproj/motor.py
@@ -23,6 +23,7 @@ class Motor(planetproj.PlanetProj):
         self.i2c = planetproj.I2C()
 
     def set_power(self, n, power):
+        assert(0 <= n < self.num_devs)
         assert(0 <= power <= 1)
         self._write_with_cs(n, planetproj.CMD_SET_POWER,
                 [0, int(round(power * 255))])
@@ -30,30 +31,40 @@ class Motor(planetproj.PlanetProj):
                 [1, int(round(power * 255))])
 
     def set_zero_position(self, n):
+        assert(0 <= n < self.num_devs)
         self.cur_pos[n] = 0
 
     def get_current_degree(self, n):
+        assert(0 <= n < self.num_devs)
         return self.cur_pos[n] * self.degrees_per_step
 
     def do_rotate_step_relative(self, n, step):
+        assert(0 <= n < self.num_devs)
+        if step == 0:
+            return
         self.cur_pos[n] += step
         if step < 0:
             is_back = 1
             step = -step
         else:
             is_back = 0
-        assert(step >= 0)
         print('Rotating', '-' if is_back else '+', step, 'steps')
         self._write_with_cs(n, planetproj.CMD_SET_ROTATE,
                 [is_back, step & 0xff, step >> 8], wait = 1)
 
     def do_rotate_degree_relative(self, n, degree):
+        assert(0 <= n < self.num_devs)
+        if degree == 0:
+            return
         # We don't have to concern reduction ratio here because it's handled by
         # interval_gen.py.
         step = int(round(degree / self.degrees_per_step))
         self.do_rotate_step_relative(n, step)
 
     def do_rotate_degree_absolute(self, n, degree):
+        assert(0 <= n < self.num_devs)
+        if degree == 0:
+            return
         cur_degree = self.get_current_degree(n)
         step = int(round((degree - cur_degree) / self.degrees_per_step))
         self.do_rotate_step_relative(n, step)
